b1d056496f0946c7f05fa1c2f47fba7855f53f95
cqxg <cqxg@yandex.ru>
2021-05-23 19:51:04 +0300

    feat: usercontroller refactoring

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,8 +1,8 @@
-var router = Router();
-var bcrypt = require('bcrypt');
-var jwt = require('jsonwebtoken');
+const router = require('express').Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-var User = require('../db').import('../models/user');
+const User = require('../db').import('../models/user');
 
 router.post('/signup', (req, res) => {
     User.create({
@@ -31,7 +31,7 @@ router.post('/signin', (req, res) => {
         if (user) {
             bcrypt.compare(req.body.user.password, user.passwordHash, function (err, matches) {
                 if (matches) {
-                    var token = jwt.sign({ id: user.id }, 'lets_play_sum_games_man', { expiresIn: 60 * 60 * 24 });
+                    const token = jwt.sign({ id: user.id }, 'lets_play_sum_games_man', { expiresIn: 60 * 60 * 24 });
                     res.json({
                         user: user,
                         message: "Successfully authenticated.",
