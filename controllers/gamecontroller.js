504c700e57639c8835f8d757e85e67fd2bb36184
cqxg <cqxg@yandex.ru>
2021-05-23 20:06:52 +0300

    feat: add gamecontroller refactoring

--- controllers/gamecontroller.js
+++ controllers/gamecontroller.js
@@ -1,5 +1,6 @@
-var router = require('express').Router();
-var Game = require('../models/game');
+const router = require('express').Router();
+
+const Game = require('../models/game');
 
 router.get('/all', (req, res) => {
     Game.findAll({ where: { owner_id: req.user.id } })
